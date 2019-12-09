363b5080a838e21c05e47717c2254a767e486a5c
Adam Kowalski <adam.kowalski.work@gmail.com>
2019-12-08 19:17:05 -0800

    switch to index based approach instead of pointers

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,5 @@
 const std = @import("std");
 
-pub fn Tensor(comptime T: type) type {
-    return struct {
-        shape: []const u64,
-    };
-}
-
 pub fn Constant(comptime T: type) type {
     return struct {
         value: T,
@@ -16,6 +10,13 @@ pub fn Operation(comptime T: type) type {
     return struct {
         left: Tensor(T),
         right: Tensor(T),
+    };
+}
+
+pub fn Tensor(comptime T: type) type {
+    return union(enum) {
+        constant: u64,
+        operation: u64,
     };
 }
 
@@ -37,16 +38,31 @@ pub fn Graph(comptime T: type) type {
 
 pub fn constant(graph: var, value: var) !Tensor(@typeOf(graph.*).elementType) {
     const T = @typeOf(graph.*).elementType;
-    try graph.constants.append(.{ .value = value });
-    return Tensor(T){ .shape = &[_]u64{} };
+    const c = try graph.constants.addOne();
+    c.* = .{ .value = value };
+    return Tensor(T){ .constant = graph.constants.count() - 1 };
+}
+
+test "constant" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = &arena.allocator;
+
+    var graph = Graph(f64).init(allocator);
+    const x = try constant(&graph, 5);
+    const y = try constant(&graph, 10);
+    std.testing.expectEqual(graph.constants.at(x.constant).value, 5);
+    std.testing.expectEqual(graph.constants.at(y.constant).value, 10);
 }
 
 pub fn add(graph: var, x: var, y: @typeOf(x)) !@typeOf(x) {
-    try graph.operations.append(.{ .left = x, .right = y });
-    return @typeOf(x){ .shape = &[_]u64{} };
+    const T = @typeOf(graph.*).elementType;
+    const o = try graph.operations.addOne();
+    o.* = .{ .left = x, .right = y };
+    return Tensor(T){ .operation = graph.operations.count() - 1 };
 }
 
-test "create graph" {
+test "add" {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = &arena.allocator;
@@ -55,6 +71,8 @@ test "create graph" {
     const x = try constant(&graph, 5);
     const y = try constant(&graph, 10);
     const z = try add(&graph, x, y);
+    std.testing.expectEqual(graph.constants.at(x.constant), graph.constants.at(graph.operations.at(z.operation).left.constant));
+    std.testing.expectEqual(graph.constants.at(y.constant), graph.constants.at(graph.operations.at(z.operation).right.constant));
 }
 
 pub fn main() !void {}
