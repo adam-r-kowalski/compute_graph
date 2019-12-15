9461baf9948e0f20f69115de2c0ba8da21e799c4
Adam Kowalski <adam.kowalski.work@gmail.com>
2019-12-14 20:51:22 -0800

    Tensors store a node which is not parameterized by type.
    This will allow for a union(enum) over the different primitive types for
    polymorphic operations

--- src/main.zig
+++ src/main.zig
@@ -1,152 +1,144 @@
 const std = @import("std");
 
-pub fn Tensor(comptime T: type) type {
-    return union(enum) {
-        constant: u64,
-        operation: u64,
-    };
-}
-
-pub fn Graph(comptime T: type) type {
+pub fn Tensor(comptime T: type, comptime rank: u64) type {
     return struct {
-        arena: *std.heap.ArenaAllocator,
-        constants: std.ArrayList(Constant(T)),
-        operations: std.ArrayList(*const Operation(T)),
-
-        pub const elementType: type = T;
-
-        pub fn init(allocator: *std.mem.Allocator) !Graph(T) {
-            const arena = try allocator.create(std.heap.ArenaAllocator);
-            arena.* = std.heap.ArenaAllocator.init(allocator);
-            return Graph(T){
-                .arena = arena,
-                .constants = std.ArrayList(Constant(T)).init(&arena.allocator),
-                .operations = std.ArrayList(*const Operation(T)).init(&arena.allocator),
-            };
-        }
-
-        pub fn deinit(self: *Graph(T)) void {
-            const child_allocator = self.arena.child_allocator;
-            self.arena.deinit();
-            child_allocator.destroy(self.arena);
-        }
+        node: Node,
     };
 }
 
-pub fn Constant(comptime T: type) type {
-    return struct {
-        value: T,
-    };
-}
+const Node = union(enum) {
+    constant: u64,
+    operation: u64,
+};
 
-pub fn constant(graph: var, value: var) !Tensor(@typeOf(graph.*).elementType) {
-    const T = @typeOf(graph.*).elementType;
+const Graph = struct {
+    arena: *std.heap.ArenaAllocator,
+    constants: std.ArrayList(Constant),
+    operations: std.ArrayList(*const Operation),
+
+    pub fn init(allocator: *std.mem.Allocator) !Graph {
+        const arena = try allocator.create(std.heap.ArenaAllocator);
+        arena.* = std.heap.ArenaAllocator.init(allocator);
+        return Graph{
+            .arena = arena,
+            .constants = std.ArrayList(Constant).init(&arena.allocator),
+            .operations = std.ArrayList(*const Operation).init(&arena.allocator),
+        };
+    }
+
+    pub fn deinit(self: *Graph) void {
+        const child_allocator = self.arena.child_allocator;
+        self.arena.deinit();
+        child_allocator.destroy(self.arena);
+    }
+};
+
+const Constant = struct {
+    value: f64,
+};
+
+pub fn constant(graph: var, value: var) !Tensor(f64, 0) {
     try graph.constants.append(.{ .value = value });
-    return Tensor(T){ .constant = graph.constants.count() - 1 };
+    const node = Node{ .constant = graph.constants.len - 1 };
+    return Tensor(f64, 0){ .node = node };
 }
 
 test "constant" {
-    var graph = try Graph(f64).init(std.heap.page_allocator);
+    var graph = try Graph.init(std.heap.page_allocator);
     defer graph.deinit();
     const x = try constant(&graph, 5);
     const y = try constant(&graph, 10);
 
-    std.testing.expectEqual(graph.constants.at(x.constant).value, 5);
-    std.testing.expectEqual(graph.constants.at(y.constant).value, 10);
+    std.testing.expectEqual(graph.constants.at(x.node.constant).value, 5);
+    std.testing.expectEqual(graph.constants.at(y.node.constant).value, 10);
 }
 
-pub fn Operation(comptime T: type) type {
-    return struct {
-        inputs: fn (self: *const Operation(T)) []const Tensor(T),
-    };
-}
+const Operation = struct {
+    inputs: fn (self: *const Operation) []const Node,
+};
 
-pub fn Add(comptime T: type) type {
-    return struct {
-        operation: Operation(T),
-        left: Tensor(T),
-        right: Tensor(T),
+const Add = struct {
+    operation: Operation,
+    left: Node,
+    right: Node,
 
-        pub fn init(left: Tensor(T), right: Tensor(T)) Add(T) {
-            return .{
-                .operation = .{ .inputs = inputs },
-                .left = left,
-                .right = right,
-            };
-        }
+    pub fn init(left: Tensor(f64, 0), right: Tensor(f64, 0)) Add {
+        return .{
+            .operation = .{ .inputs = inputs },
+            .left = left.node,
+            .right = right.node,
+        };
+    }
 
-        pub fn inputs(operation: *const Operation(T)) []const Tensor(T) {
-            const self = @fieldParentPtr(Add(T), "operation", operation);
-            return &[_]Tensor(T){ self.left, self.right };
-        }
-    };
-}
+    pub fn inputs(operation: *const Operation) []const Node {
+        const self = @fieldParentPtr(Add, "operation", operation);
+        return &[_]Node{ self.left, self.right };
+    }
+};
 
-pub fn add(graph: var, x: var, y: @typeOf(x)) !@typeOf(x) {
-    const T = @typeOf(graph.*).elementType;
-    var a = try graph.arena.allocator.create(Add(T));
-    a.* = Add(T).init(x, y);
+pub fn add(graph: var, x: var, y: @TypeOf(x)) !@TypeOf(x) {
+    var a = try graph.arena.allocator.create(Add);
+    a.* = Add.init(x, y);
     try graph.operations.append(&a.operation);
-    return Tensor(T){ .operation = graph.operations.count() - 1 };
+    const node = Node{ .operation = graph.operations.len - 1 };
+    return Tensor(f64, 0){ .node = node };
 }
 
 test "add" {
-    var graph = try Graph(f64).init(std.heap.page_allocator);
+    var graph = try Graph.init(std.heap.page_allocator);
     defer graph.deinit();
     const x = try constant(&graph, 5);
     const y = try constant(&graph, 10);
     const z = try add(&graph, x, y);
 
-    const operation = graph.operations.at(z.operation);
-    const a = @fieldParentPtr(Add(f64), "operation", operation);
+    const operation = graph.operations.at(z.node.operation);
+    const a = @fieldParentPtr(Add, "operation", operation);
     const left = graph.constants.at(a.left.constant);
     const right = graph.constants.at(a.right.constant);
-    std.testing.expectEqual(graph.constants.at(x.constant), left);
-    std.testing.expectEqual(graph.constants.at(y.constant), right);
+    std.testing.expectEqual(graph.constants.at(x.node.constant), left);
+    std.testing.expectEqual(graph.constants.at(y.node.constant), right);
 }
 
-pub fn Multiply(comptime T: type) type {
-    return struct {
-        operation: Operation(T),
-        left: Tensor(T),
-        right: Tensor(T),
+const Multiply = struct {
+    operation: Operation,
+    left: Node,
+    right: Node,
 
-        pub fn init(left: Tensor(T), right: Tensor(T)) Multiply(T) {
-            return .{
-                .operation = .{ .inputs = inputs },
-                .left = left,
-                .right = right,
-            };
-        }
+    pub fn init(left: Tensor(f64, 0), right: Tensor(f64, 0)) Multiply {
+        return .{
+            .operation = .{ .inputs = inputs },
+            .left = left.node,
+            .right = right.node,
+        };
+    }
 
-        pub fn inputs(operation: *const Operation(T)) []const Tensor(T) {
-            const self = @fieldParentPtr(Multiply(T), "operation", operation);
-            return &[_]Tensor(T){ self.left, self.right };
-        }
-    };
-}
+    pub fn inputs(operation: *const Operation) []const Node {
+        const self = @fieldParentPtr(Multiply, "operation", operation);
+        return &[_]Node{ self.left, self.right };
+    }
+};
 
-pub fn multiply(graph: var, x: var, y: @typeOf(x)) !@typeOf(x) {
-    const T = @typeOf(graph.*).elementType;
-    var a = try graph.arena.allocator.create(Multiply(T));
-    a.* = Multiply(T).init(x, y);
+pub fn multiply(graph: var, x: var, y: @TypeOf(x)) !@TypeOf(x) {
+    var a = try graph.arena.allocator.create(Multiply);
+    a.* = Multiply.init(x, y);
     try graph.operations.append(&a.operation);
-    return Tensor(T){ .operation = graph.operations.count() - 1 };
+    const node = Node{ .operation = graph.operations.len - 1 };
+    return Tensor(f64, 0){ .node = node };
 }
 
 test "multiply" {
-    var graph = try Graph(f64).init(std.heap.page_allocator);
+    var graph = try Graph.init(std.heap.page_allocator);
     defer graph.deinit();
     const x = try constant(&graph, 5);
     const y = try constant(&graph, 10);
     const z = try multiply(&graph, x, y);
 
-    const operation = graph.operations.at(z.operation);
-    const a = @fieldParentPtr(Multiply(f64), "operation", operation);
+    const operation = graph.operations.at(z.node.operation);
+    const a = @fieldParentPtr(Multiply, "operation", operation);
     const left = graph.constants.at(a.left.constant);
     const right = graph.constants.at(a.right.constant);
-    std.testing.expectEqual(graph.constants.at(x.constant), left);
-    std.testing.expectEqual(graph.constants.at(y.constant), right);
+    std.testing.expectEqual(graph.constants.at(x.node.constant), left);
+    std.testing.expectEqual(graph.constants.at(y.node.constant), right);
 }
 
 pub fn main() !void {}
