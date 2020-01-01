42ec7a1189286993255e0d07ffd222b67fa668fe
Adam Kowalski <adam.kowalski.work@gmail.com>
2020-01-01 01:01:52 -0800

    compute shape of tensor

--- src/cpu_tensor.zig
+++ src/cpu_tensor.zig
@@ -52,10 +52,14 @@ test "array info rank 3" {
     std.testing.expectEqual(info, ArrayInfo{ .rank = 3, .child = f16 });
 }
 
-pub fn CpuTensor(comptime ScalarType: type, comptime rank: u64) type {
+pub fn CpuTensor(comptime T: type, comptime r: u64) type {
     return struct {
+        const ScalarType = T;
+        const rank = r;
+
+        shape: [rank]u64,
         stride: [rank]u64,
-        data: []ScalarType,
+        data: []const ScalarType,
     };
 }
 
@@ -64,12 +68,59 @@ fn tensorType(comptime T: type) type {
     return CpuTensor(info.child, info.rank);
 }
 
-pub fn cpuTensor(literal: var) tensorType(@TypeOf(literal)) {
-    const T = tensorType(@TypeOf(literal));
-    return T{ .stride = undefined, .data = literal };
+fn tensorShape(comptime rank: u64, literal: var) [rank]u64 {
+    var shape: [rank]u64 = undefined;
+    const Closure = struct {
+        fn call(s: *[rank]u64, i: u64, l: var) void {
+            switch (@typeInfo(@TypeOf(l))) {
+                .Pointer, .Array => {
+                    s[i] = l.len;
+                    call(s, i + 1, l[0]);
+                },
+                else => {},
+            }
+        }
+    };
+    Closure.call(&shape, 0, literal);
+    return shape;
 }
 
-test "cpu tensor" {
+pub fn cpuTensor(literal: var) tensorType(@TypeOf(literal)) {
+    const T = tensorType(@TypeOf(literal));
+    return T{
+        .shape = tensorShape(T.rank, literal),
+        .stride = undefined,
+        .data = undefined,
+    };
+}
+
+test "cpu tensor rank 1" {
     const tensor = cpuTensor(&[_]f64{ 1, 2, 3 });
-    std.testing.expect(std.mem.eql(f64, tensor.data, &[_]f64{ 1, 2, 3 }));
+    std.testing.expect(std.mem.eql(u64, tensor.shape[0..], &[_]u64{3}));
+}
+
+test "cpu tensor rank 2" {
+    const tensor = cpuTensor(&[_][3]i32{
+        .{ 1, 2, 3 },
+        .{ 4, 5, 6 },
+    });
+    std.testing.expect(std.mem.eql(u64, tensor.shape[0..], &[_]u64{ 2, 3 }));
+}
+
+test "cpu tensor rank 3" {
+    const tensor = cpuTensor(&[_][2][3]f16{
+        .{
+            .{ 1, 2, 3 },
+            .{ 4, 5, 6 },
+        },
+        .{
+            .{ 7, 8, 9 },
+            .{ 10, 11, 12 },
+        },
+        .{
+            .{ 13, 14, 15 },
+            .{ 16, 17, 18 },
+        },
+    });
+    std.testing.expect(std.mem.eql(u64, tensor.shape[0..], &[_]u64{ 3, 2, 3 }));
 }
