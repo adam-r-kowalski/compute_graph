4933838afd26e0924239d8a0818a2856ee211e6b
Adam Kowalski <adam.kowalski.work@gmail.com>
2020-01-25 16:08:11 -0800

    implement backward for add and multiply

--- src/eager/add.zig
+++ src/eager/add.zig
@@ -3,6 +3,7 @@ const Allocator = std.mem.Allocator;
 const constant = @import("constant.zig").constant;
 const CpuTensor = @import("cpu_tensor.zig").CpuTensor;
 const expectEqual = @import("../testing.zig").expectEqual;
+const backward = @import("backward.zig");
 
 pub fn add(comptime T: type, allocator: *Allocator, x: CpuTensor(T), y: CpuTensor(T)) !CpuTensor(T) {
     if (!std.mem.eql(usize, x.shape, y.shape))
@@ -92,4 +93,73 @@ test "add rank 3" {
         },
     });
     expectEqual(i8, actual, expected);
+}
+
+pub fn add_backward(comptime T: type, context: backward.Context(T)) ![]CpuTensor(T) {
+    std.debug.assert(context.forward_inputs.len == 2);
+    const outputs = try context.allocator.alloc(CpuTensor(T), 2);
+    errdefer context.allocator.free(outputs);
+    outputs[0] = context.gradient_input;
+    outputs[1] = context.gradient_input;
+    return outputs;
+}
+
+test "add backward rank 0" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const x = try constant(&arena.allocator, @as(f64, 4));
+    const y = try constant(&arena.allocator, @as(f64, 10));
+    const gradient_input = try constant(&arena.allocator, @as(f64, 1));
+    const actual = try add_backward(f64, backward.Context(f64){
+        .allocator = &arena.allocator,
+        .gradient_input = gradient_input,
+        .forward_inputs = &[_]CpuTensor(f64){x, y},
+    });
+    const expected = try constant(&arena.allocator, @as(f64, 1));
+    expectEqual(f64, actual[0], expected);
+    expectEqual(f64, actual[1], expected);
+}
+
+test "add backward rank 1" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const x = try constant(&arena.allocator, [_]f64{ 1, 2, 3, 4, 5 });
+    const y = try constant(&arena.allocator, [_]f64{ 6, 7, 8, 9, 10 });
+    const gradient_input = try constant(&arena.allocator,  [_]f64{ 2, 4, 6, 8, 10 });
+    const actual = try add_backward(f64, backward.Context(f64){
+        .allocator = &arena.allocator,
+        .gradient_input = gradient_input,
+        .forward_inputs = &[_]CpuTensor(f64){x, y},
+    });
+    const expected = try constant(&arena.allocator,  [_]f64{ 2, 4, 6, 8, 10 });
+    expectEqual(f64, actual[0], expected);
+    expectEqual(f64, actual[1], expected);
+}
+
+test "add backward rank 2" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const x = try constant(&arena.allocator, [_][2]f64{
+        .{ 1, 2 },
+        .{ 3, 4 },
+    });
+    const y = try constant(&arena.allocator, [_][2]f64{
+        .{ 5, 6 },
+        .{ 7, 8 },
+    });
+    const gradient_input = try constant(&arena.allocator, [_][2]f64{
+        .{ 2, 4 },
+        .{ 6, 8 },
+    });
+    const actual = try add_backward(f64, backward.Context(f64){
+        .allocator = &arena.allocator,
+        .gradient_input = gradient_input,
+        .forward_inputs = &[_]CpuTensor(f64){x, y},
+    });
+    const expected = try constant(&arena.allocator, [_][2]f64{
+        .{ 2, 4 },
+        .{ 6, 8 },
+    });
+    expectEqual(f64, actual[0], expected);
+    expectEqual(f64, actual[1], expected);
 }
