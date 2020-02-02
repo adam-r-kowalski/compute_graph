8ed64949968336e1b091536c70f922339982e802
Adam Kowalski <adam.kowalski.work@gmail.com>
2020-02-01 23:20:58 -0800

    linear regression example

--- src/lazy/assign.zig
+++ src/lazy/assign.zig
@@ -66,3 +66,49 @@ test "assign" {
     expectEqual(f64, actual3[0].f64, expected3);
     expectEqual(f64, actual3[1].f64, expected3);
 }
+
+test "linear regression" {
+    const add = @import("add.zig").add;
+    const absolute = @import("absolute.zig").absolute;
+    const multiply = @import("multiply.zig").multiply;
+    const subtract = @import("subtract.zig").subtract;
+    const constant = @import("constant.zig").constant;
+    const variable = @import("variable.zig").variable;
+    const gradient = @import("gradient.zig").gradient;
+    const eager = @import("../eager.zig");
+    const allocator = std.heap.page_allocator;
+    var arena = std.heap.ArenaAllocator.init(allocator);
+    defer arena.deinit();
+    var graph = try Graph.init(allocator);
+    defer graph.deinit();
+    const m = try variable(&graph, try constant(&graph, @as(f64, 8)));
+    const b = try variable(&graph, try constant(&graph, @as(f64, 6)));
+    const x = try constant(&graph, @as(f64, 2));
+    const y = try constant(&graph, @as(f64, 4));
+    const y_hat = try add(&graph, try multiply(&graph, m, x), b);
+    const loss = try absolute(&graph, try subtract(&graph, y, y_hat));
+    const gradients = try gradient(&graph, loss, &[_]Tensor{ m, b });
+    const step_size = try constant(&graph, @as(f64, 0.3));
+    const dm = try multiply(&graph, gradients[0], step_size);
+    const db = try multiply(&graph, gradients[1], step_size);
+    const improve_m = try assign(&graph, m, try subtract(&graph, m, dm));
+    const improve_b = try assign(&graph, b, try subtract(&graph, b, db));
+    var session = try Session.init(allocator, &graph);
+    defer session.deinit();
+
+    const actual = try session.run(&[_]Tensor{loss});
+    const actual_loss = actual[0];
+    expectEqual(f64, actual_loss.f64, try eager.constant(&arena.allocator, @as(f64, 1.8e+01)));
+
+    var i: usize = 0;
+    while (i < 100) : (i += 1)
+        _ = try session.run(&[_]Tensor{ improve_m, improve_b });
+
+    const actual1 = try session.run(&[_]Tensor{ loss, m, b });
+    const actual_loss1 = actual1[0];
+    const actual_m1 = actual1[1];
+    const actual_b1 = actual1[2];
+    expectEqual(f64, actual_loss1.f64, try eager.constant(&arena.allocator, @as(f64, 5.329070518200751e-15)));
+    expectEqual(f64, actual_m1.f64, try eager.constant(&arena.allocator, @as(f64, 8.000000000000017e-01)));
+    expectEqual(f64, actual_b1.f64, try eager.constant(&arena.allocator, @as(f64, 2.400000000000002)));
+}
