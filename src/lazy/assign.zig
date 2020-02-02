dcc44d4d85daacf361c48d53d3fa266acdcdfad8
Adam Kowalski <adam.kowalski.work@gmail.com>
2020-02-01 17:15:23 -0800

    assignment works when applied twice

--- src/lazy/assign.zig
+++ src/lazy/assign.zig
@@ -41,18 +41,28 @@ test "assign" {
     const e = try assign(&graph, b, d);
     var session = try Session.init(allocator, &graph);
     defer session.deinit();
+
     const actual1 = try session.run(&[_]Tensor{ e, b });
     const expected1 = try eager.constant(&arena.allocator, [_][2]f64{
         .{ 2, 3 },
         .{ 4, 5 },
     });
+    expectEqual(f64, actual1[0].f64, expected1);
+    expectEqual(f64, actual1[1].f64, expected1);
+
     const actual2 = try session.run(&[_]Tensor{ e, b });
     const expected2 = try eager.constant(&arena.allocator, [_][2]f64{
         .{ 3, 4 },
         .{ 5, 6 },
     });
-    expectEqual(f64, actual1[0].f64, expected1);
-    expectEqual(f64, actual1[1].f64, expected1);
     expectEqual(f64, actual2[0].f64, expected2);
     expectEqual(f64, actual2[1].f64, expected2);
+
+    const actual3 = try session.run(&[_]Tensor{ e, b });
+    const expected3 = try eager.constant(&arena.allocator, [_][2]f64{
+        .{ 4, 5 },
+        .{ 6, 7 },
+    });
+    expectEqual(f64, actual3[0].f64, expected3);
+    expectEqual(f64, actual3[1].f64, expected3);
 }
