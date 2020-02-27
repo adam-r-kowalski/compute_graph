182009d5d5d3e208e719365f8155bb56b89e69d3
Adam Kowalski <adam.kowalski.work@gmail.com>
2020-02-26 20:56:01 -0800

    implement broadcast scalar for add forward

--- src/eager/add.zig
+++ src/eager/add.zig
@@ -5,9 +5,7 @@ const CpuTensor = @import("cpu_tensor.zig").CpuTensor;
 const expectEqual = @import("../testing.zig").expectEqual;
 const backward = @import("backward.zig");
 
-pub fn add(comptime T: type, allocator: *Allocator, x: CpuTensor(T), y: CpuTensor(T)) !CpuTensor(T) {
-    if (!std.mem.eql(usize, x.shape, y.shape))
-        return error.ShapeMismatch;
+fn addSameShape(comptime T: type, allocator: *Allocator, x: CpuTensor(T), y: CpuTensor(T)) !CpuTensor(T) {
     const shape = x.shape;
     const stride = x.stride;
     switch (x.storage) {
@@ -30,6 +28,30 @@ pub fn add(comptime T: type, allocator: *Allocator, x: CpuTensor(T), y: CpuTenso
             };
         },
     }
+}
+
+fn addBroadcastScalar(comptime T: type, allocator: *Allocator, scalar: T, tensor: CpuTensor(T)) !CpuTensor(T) {
+    const shape = tensor.shape;
+    const stride = tensor.stride;
+    const array = tensor.storage.array;
+    const new_array = try allocator.alloc(T, array.len);
+    errdefer allocator.free(new_array);
+    for (array) |e, i| new_array[i] = scalar + array[i];
+    return CpuTensor(T){
+        .shape = shape,
+        .stride = stride,
+        .storage = .{ .array = new_array },
+    };
+}
+
+pub fn add(comptime T: type, allocator: *Allocator, x: CpuTensor(T), y: CpuTensor(T)) !CpuTensor(T) {
+    if (std.mem.eql(usize, x.shape, y.shape))
+        return try addSameShape(T, allocator, x, y);
+    if (x.shape.len == 0)
+        return try addBroadcastScalar(T, allocator, x.storage.scalar, y);
+    if (y.shape.len == 0)
+        return try addBroadcastScalar(T, allocator, y.storage.scalar, x);
+    return error.ShapeMismatch;
 }
 
 test "add rank 0" {
@@ -93,6 +115,68 @@ test "add rank 3" {
         },
     });
     expectEqual(i8, actual, expected);
+}
+
+test "add broadcast scalar rank 1" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const scalar = try constant(i32, &arena.allocator, 5);
+    const tensor = try constant(i32, &arena.allocator, .{ 1, -2, 3, -4, -5, 6 });
+    const actual = try add(i32, &arena.allocator, scalar, tensor);
+    const actual2 = try add(i32, &arena.allocator, tensor, scalar);
+    const expected = try constant(i32, &arena.allocator, .{ 6, 3, 8, 1, 0, 11 });
+    expectEqual(i32, actual, expected);
+    expectEqual(i32, actual2, expected);
+}
+
+test "add broadcast scalar rank 2" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const scalar = try constant(f16, &arena.allocator, 3);
+    const tensor = try constant(f16, &arena.allocator, .{
+        .{ 1, -2 },
+        .{ 3, -4 },
+        .{ -5, 6 },
+    });
+    const actual = try add(f16, &arena.allocator, scalar, tensor);
+    const actual2 = try add(f16, &arena.allocator, tensor, scalar);
+    const expected = try constant(f16, &arena.allocator, .{
+        .{ 4, 1 },
+        .{ 6, -1 },
+        .{ -2, 9 },
+    });
+    expectEqual(f16, actual, expected);
+    expectEqual(f16, actual2, expected);
+}
+
+test "add broadcast scalar rank 3" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const scalar = try constant(i8, &arena.allocator, -5);
+    const tensor = try constant(i8, &arena.allocator, .{
+        .{
+            .{ 1, -2 },
+            .{ 3, -4 },
+        },
+        .{
+            .{ 5, -6 },
+            .{ 7, -8 },
+        },
+    });
+    const actual = try add(i8, &arena.allocator, scalar, tensor);
+    const actual2 = try add(i8, &arena.allocator, tensor, scalar);
+    const expected = try constant(i8, &arena.allocator, .{
+        .{
+            .{ -4, -7 },
+            .{ -2, -9 },
+        },
+        .{
+            .{ 0, -11 },
+            .{ 2, -13 },
+        },
+    });
+    expectEqual(i8, actual, expected);
+    expectEqual(i8, actual2, expected);
 }
 
 pub fn addBackward(comptime T: type, context: backward.Context(T)) ![]CpuTensor(T) {
