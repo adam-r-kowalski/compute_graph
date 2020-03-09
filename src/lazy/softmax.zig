dcac7997280c1026f4083776c82c5836c2556585
Adam Kowalski <adam.kowalski.work@gmail.com>
2020-03-08 17:10:23 -0700

    every library I've checked gives different answer for softmax gradient

--- src/lazy/softmax.zig
+++ src/lazy/softmax.zig
@@ -5,21 +5,29 @@ const exponentiate = @import("exponentiate.zig").exponentiate;
 const sum = @import("sum.zig").sum;
 const divide = @import("divide.zig").divide;
 const expectEqual = @import("../testing.zig").expectEqual;
+const constant = @import("constant.zig").constant;
+const Session = @import("session.zig").Session;
+const gradient = @import("gradient.zig").gradient;
+const mean = @import("mean.zig").mean;
+const eager = @import("../eager.zig");
 
 const SoftmaxParameters = struct {
     dimension: ?usize = null,
 };
 
 fn softmax(graph: *Graph, x: Tensor, parameters: SoftmaxParameters) !Tensor {
+    // TODO: refactor to this
+    // const a = try maximum(graph, x, .{ .dimension = parameters.dimension });
+    // const b = try subtract(graph, x, a);
+    // const c = try exponentiate(graph, b);
+    // const d = try sum(graph, c, .{ .dimension = parameters.dimension });
+    // return try divide(graph, c, d);
     const a = try exponentiate(graph, x);
     const b = try sum(graph, a, .{ .dimension = parameters.dimension });
     return try divide(graph, a, b);
 }
 
 test "softmax vector" {
-    const constant = @import("constant.zig").constant;
-    const Session = @import("session.zig").Session;
-    const eager = @import("../eager.zig");
     const allocator = std.heap.page_allocator;
     var arena = std.heap.ArenaAllocator.init(allocator);
     defer arena.deinit();
@@ -28,50 +36,19 @@ test "softmax vector" {
     const a = try constant(f64, &graph, .{ 0.1, 0.2, 0.3, 0.4, 0.5 });
     const b = try softmax(&graph, a, .{});
     const c = try sum(&graph, b, .{});
+    const d = try gradient(&graph, c, &[_]Tensor{a});
     std.testing.expect(std.mem.eql(usize, b.shape, &[_]usize{5}));
     var session = try Session.init(allocator, &graph);
     defer session.deinit();
-    const actual = try session.run(&[_]Tensor{ b, c }, .{});
+    const actual = try session.run(&[_]Tensor{ b, c, d[0] }, .{});
     const expected = try eager.constant(f64, &arena.allocator, .{ 0.1621, 0.1792, 0.1980, 0.2188, 0.2419 });
     const expected2 = try eager.constant(f64, &arena.allocator, 1);
     expectEqual(f64, actual[0].f64, expected);
     expectEqual(f64, actual[1].f64, expected2);
-}
-
-test "softmax matrix dimension 0" {
-    const constant = @import("constant.zig").constant;
-    const Session = @import("session.zig").Session;
-    const eager = @import("../eager.zig");
-    const allocator = std.heap.page_allocator;
-    var arena = std.heap.ArenaAllocator.init(allocator);
-    defer arena.deinit();
-    var graph = try Graph.init(allocator);
-    defer graph.deinit();
-    const a = try constant(f64, &graph, .{
-        .{ 0.1, 0.2, 0.3, 0.4, 0.5 },
-        .{ -0.1, -0.2, -0.3, -0.4, -0.5 },
-    });
-    const b = try softmax(&graph, a, .{ .dimension = 0 });
-    const c = try sum(&graph, b, .{ .dimension = 0 });
-    std.testing.expect(std.mem.eql(usize, b.shape, &[_]usize{ 2, 5 }));
-    var session = try Session.init(allocator, &graph);
-    defer session.deinit();
-    const actual = try session.run(&[_]Tensor{ b, c }, .{});
-    const expected = try eager.constant(f64, &arena.allocator, .{
-        .{ 0.5498, 0.5987, 0.6457, 0.69, 0.7311 },
-        .{ 0.4502, 0.4013, 0.3543, 0.31, 0.2689 },
-    });
-    const expected2 = try eager.constant(f64, &arena.allocator, .{ 1, 1, 1, 1, 1 });
-    expectEqual(f64, actual[0].f64, expected);
-    expectEqual(f64, actual[1].f64, expected2);
+    std.debug.warn("\n{}\n", .{actual[2]});
 }
 
 test "gradient softmax matrix dimension 0" {
-    const constant = @import("constant.zig").constant;
-    const Session = @import("session.zig").Session;
-    const gradient = @import("gradient.zig").gradient;
-    const mean = @import("mean.zig").mean;
-    const eager = @import("../eager.zig");
     const allocator = std.heap.page_allocator;
     var arena = std.heap.ArenaAllocator.init(allocator);
     defer arena.deinit();
