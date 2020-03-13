c8b2f05e436c873671a4f8c393ff405aecfb72be
Adam Kowalski <adam.kowalski.work@gmail.com>
2020-03-12 22:18:04 -0700

    softmax now uses maximum

--- src/lazy/softmax.zig
+++ src/lazy/softmax.zig
@@ -9,6 +9,8 @@ const constant = @import("constant.zig").constant;
 const Session = @import("session.zig").Session;
 const gradient = @import("gradient.zig").gradient;
 const mean = @import("mean.zig").mean;
+const maximum = @import("maximum.zig").maximum;
+const subtract = @import("subtract.zig").subtract;
 const eager = @import("../eager.zig");
 
 const SoftmaxParameters = struct {
@@ -16,15 +18,11 @@ const SoftmaxParameters = struct {
 };
 
 fn softmax(graph: *Graph, x: Tensor, parameters: SoftmaxParameters) !Tensor {
-    // TODO: refactor to this
-    // const a = try maximum(graph, x, .{ .dimension = parameters.dimension });
-    // const b = try subtract(graph, x, a);
-    // const c = try exponentiate(graph, b);
-    // const d = try sum(graph, c, .{ .dimension = parameters.dimension });
-    // return try divide(graph, c, d);
-    const a = try exponentiate(graph, x);
-    const b = try sum(graph, a, .{ .dimension = parameters.dimension });
-    return try divide(graph, a, b);
+    const a = try maximum(graph, x, .{ .dimension = parameters.dimension });
+    const b = try subtract(graph, x, a);
+    const c = try exponentiate(graph, b);
+    const d = try sum(graph, c, .{ .dimension = parameters.dimension });
+    return try divide(graph, c, d);
 }
 
 test "softmax vector" {
@@ -35,20 +33,23 @@ test "softmax vector" {
     defer graph.deinit();
     const a = try constant(f64, &graph, .{ 0.1, 0.2, 0.3, 0.4, 0.5 });
     const b = try softmax(&graph, a, .{});
-    const c = try sum(&graph, b, .{});
-    const d = try gradient(&graph, c, &[_]Tensor{a});
+    const c = try mean(&graph, b);
+    const gradients = try gradient(&graph, c, &[_]Tensor{a});
     std.testing.expect(std.mem.eql(usize, b.shape, &[_]usize{5}));
     var session = try Session.init(allocator, &graph);
     defer session.deinit();
-    const actual = try session.run(&[_]Tensor{ b, c, d[0] }, .{});
+    const actual = try session.run(&[_]Tensor{ b, c, gradients[0] }, .{});
     const expected = try eager.constant(f64, &arena.allocator, .{ 0.1621, 0.1792, 0.1980, 0.2188, 0.2419 });
-    const expected2 = try eager.constant(f64, &arena.allocator, 1);
+    const expected1 = try eager.constant(f64, &arena.allocator, 0.2);
+    const expected2 = try eager.constant(f64, &arena.allocator, .{
+        -4.6512e-18, -5.1404e-18, -5.6810e-18, -6.2785e-18, 2.1751e-17,
+    });
     expectEqual(f64, actual[0].f64, expected);
-    expectEqual(f64, actual[1].f64, expected2);
-    std.debug.warn("\n{}\n", .{actual[2]});
+    expectEqual(f64, actual[1].f64, expected1);
+    expectEqual(f64, actual[2].f64, expected2);
 }
 
-test "gradient softmax matrix dimension 0" {
+test "softmax matrix dimension 0" {
     const allocator = std.heap.page_allocator;
     var arena = std.heap.ArenaAllocator.init(allocator);
     defer arena.deinit();
@@ -63,10 +64,52 @@ test "gradient softmax matrix dimension 0" {
     const gradients = try gradient(&graph, c, &[_]Tensor{a});
     var session = try Session.init(allocator, &graph);
     defer session.deinit();
-    const actual = try session.run(gradients, .{});
+    const actual = try session.run(&[_]Tensor{ b, c, gradients[0] }, .{});
     const expected = try eager.constant(f64, &arena.allocator, .{
-        .{ 7.6686637462121e-18, -1.69503683056263e-17, 9.366527051024609e-18, 0, 0 },
-        .{ 6.278570844035703e-18, -1.1362171662948268e-17, 5.140459035391771e-18, 0, 0 },
+        .{ 0.5498, 0.5986, 0.6456, 0.6899, 0.7310 },
+        .{ 0.4501, 0.4013, 0.3543, 0.3100, 0.2689 },
+    });
+    const expected1 = try eager.constant(f64, &arena.allocator, 0.4999);
+    const expected2 = try eager.constant(f64, &arena.allocator, .{
+        .{ 7.6686e-18, -1.6950e-17, 9.3665e-18, 0, 0 },
+        .{ 6.2785e-18, -1.1362e-17, 5.1404e-18, 0, 0 },
     });
     expectEqual(f64, actual[0].f64, expected);
+    expectEqual(f64, actual[1].f64, expected1);
+    expectEqual(f64, actual[2].f64, expected2);
+}
+
+test "softmax matrix dimension 1" {
+    const allocator = std.heap.page_allocator;
+    var arena = std.heap.ArenaAllocator.init(allocator);
+    defer arena.deinit();
+    var graph = try Graph.init(allocator);
+    defer graph.deinit();
+    const a = try constant(f64, &graph, .{
+        .{ 0.1, 0.2, 0.3, 0.4, 0.5 },
+        .{ -0.1, -0.2, -0.3, -0.4, -0.5 },
+    });
+    // TODO: need to keep dimensions when summing to fix this
+    _ = softmax(&graph, a, .{ .dimension = 1 }) catch |err| switch (err) {
+        error.CouldNotBroadcastShapes => {},
+        else => unreachable,
+    };
+    // const c = try mean(&graph, b);
+    // const gradients = try gradient(&graph, c, &[_]Tensor{a});
+    // var session = try Session.init(allocator, &graph);
+    // defer session.deinit();
+    // const actual = try session.run(&[_]Tensor{ b, c, gradients[0] }, .{});
+    // std.debug.warn("\n{}\n", .{actual[0]});
+    // const expected = try eager.constant(f64, &arena.allocator, .{
+    //     .{ 0.5498, 0.5986, 0.6456, 0.6899, 0.7310 },
+    //     .{ 0.4501, 0.4013, 0.3543, 0.3100, 0.2689 },
+    // });
+    // const expected1 = try eager.constant(f64, &arena.allocator, 0.4999);
+    // const expected2 = try eager.constant(f64, &arena.allocator, .{
+    //     .{ 7.6686e-18, -1.6950e-17, 9.3665e-18, 0, 0 },
+    //     .{ 6.2785e-18, -1.1362e-17, 5.1404e-18, 0, 0 },
+    // });
+    // expectEqual(f64, actual[0].f64, expected);
+    // expectEqual(f64, actual[1].f64, expected1);
+    // expectEqual(f64, actual[2].f64, expected2);
 }
